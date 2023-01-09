b77c96f33a7ac249c410f24b56bedba0df4c0351
DIMLll <121351837+DIMLll@users.noreply.github.com>
2023-01-09 20:23:07 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('.h').click(function () {
+        $('.i').focus();
+    });
+    $('.block3').click(function () {
+        if ($(this).html() == '<i class="fa-solid fa-eye"></i>') {
+            $(this).html('<i class="fa-sharp fa-solid fa-eye-slash"></i>');
+            $('.pass').attr('type', 'text');
+        } else {
+            $(this).html('<i class="fa-solid fa-eye"></i>');
+            $('.pass').attr('type', 'password');
+        }
+    });
+    $("*").focus(function () {
+        if ($('#r').attr('class') == 'block') {
+            $('#r').addClass('none');
+            $('#r').removeClass('block');
+        };
+    });
+    $(".pass").focus(function () {
+        $('#r').addClass('block');
+        $('#r').removeClass('none');
+    });
+});
